1d497032beced88928f51494445459299fe67af3
Ankit8125 <syncing284@gmail.com>
2024-06-11 10:26:46 +0530

    added href to user-management

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <h1>Server Actions</h1>
+      <Link href="/user-management">Go to CRUD operations for user</Link>
     </div>
   );
 }
